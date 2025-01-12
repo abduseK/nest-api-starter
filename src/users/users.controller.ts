2f6b5de55ba2bcf8a8db44cff4aa31509bcb32b3
abduseK <abdelselamkemal74@gmail.com>
2025-01-12 15:14:35 -0800

    commit before stash

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,20 +5,31 @@ import {
   Controller,
   Delete,
   Get,
+  Header,
   HttpCode,
   Param,
   Patch,
   Post,
   Query,
+  Redirect,
 } from '@nestjs/common';
 import { STATUS_CODES } from 'http';
 
 @Controller('users')
 export class UsersController {
+  // @Get()
+  // @Header(`custom`, 'val') // helps to set the status code of the response
+  // @Redirect('https://google.com', 204)
+  // findAll() {
+  //   return 'Hello There';
+  // }
+
   @Get()
-  @HttpCode(303) // helps to set the status code of the response
-  findAll() {
-    return 'Hello There';
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 
   @Get(':id')
